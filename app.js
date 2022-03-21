b2640d5a0ec28ca319ee3e6781d4f0caa50fba8e
laizhenjun <laizhenjun@skyworth.com>
2022-03-21 19:15:35 +0800

    github 测试

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+/*
+ * @Author: Zhenjun.Lai
+ * @Date: 2022-03-21 19:14:23
+ * @LastEditors: Zhenjun.Lai
+ * @LastEditTime: 2022-03-21 19:14:50
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \code_UI\miniprogram\app.js
+ */
 // app.js
 App({
   onLaunch(options) {
     // Do something initial when launch.
+    // just a test about GitHub
   },
   onShow(options) {
     // Do something when show.
