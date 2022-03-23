caccebaa5809cb286b2aa33875564375b234d73a
Ryan.lai <834250605@qq.com>
2022-03-23 23:08:42 +0800

    完善首页功能

--- src/pages/firstPage/index.js
+++ src/pages/firstPage/index.js
@@ -1,66 +1,30 @@
+/*
+ * @Author: Zhenjun.Lai
+ * @Date: 2022-03-19 16:56:59
+ * @LastEditors: Zhenjun.Lai
+ * @LastEditTime: 2022-03-23 22:40:26
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /miniprogram/src/pages/firstPage/index.js
+ */
 // pages/firstPage/index.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    swiperList: [],
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    var reqTask = wx.request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+      data: {},
+      header: { 'content-type': 'application/json' },
+      success: (result) => {
+        this.setData({
+          swiperList: result.data.message
+        })
+      },
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
+ 
